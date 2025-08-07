8a08465b04f3e14433052b250f354e8c46794027
Nirvaxstiel <nirvaxstielfujik@gmail.com>
2025-08-07 23:25:34 +0800

    fix: forgor to remove the old ApiTestApplicationFactory.

--- UniversityAPI.Tests/Shared/Fixtures/ApiTestApplicationFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace UniversityAPI.Tests.Shared.Fixtures
-{
-    public class ApiTestApplicationFactory : BaseTestApplicationFactory
-    {
-        protected override string DatabaseNamePrefix => "ApiTestDb";
-    }
-}
